a553a1d0210e51d16dd9b22a86991e19435adf5e
Dawid-Stankowski <dawid441@interia.pl>
2023-12-01 10:24:35 +0100

    1.0

--- src/ParametryProgramu.java
+++ src/ParametryProgramu.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class ParametryProgramu {
+public class ParametryProgramu {
+    public static void main(String[] args) {
+// args to parametry programu przekazane z wiersza poleceń
+// args.length to liczba przekazanych parametrów
+        System.out.println("Liczba parametrów: " + args.length);
+// Wyświetlenie wszystkich przekazanych parametrów
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Parametr " + i + ": " + args[i]);
+        }
+    }
 }
